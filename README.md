4b0876222490e0003a668038c277dfa8b45be92f
chenyouwei <chenyouwei@qupeiyin.com>
2020-07-30 15:31:16 +0800

    feat:新增调用降噪功能

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AudioFun
 
-音频操作的尝试  fmod  soundTouch
+音频操作的尝试  fmod  soundTouch webRtc
 
 
 ## SoundTouch
@@ -15,3 +15,11 @@
 将声音播放率更改为n个百分点（n = -95.0 .. +5000.0％）
 -bpm = n
 检测声音的每分钟节拍（BPM）速率，并调整速度以满足“ n”个BPM。当应用此开关时，将忽略“ -tempo”开关。如果省略“ = n”，即单独使用开关“ -bpm”，则将估算并显示BPM速率，但速度不会根据BPM值进行调整。
+
+## FMOD
+
+FMOD 修改音频参数
+
+## webRtc
+
+移植webRtc模块
--- libwebrtc/src/main/java/com/hugh/libwebrtc/RtcActivity.java
+++ libwebrtc/src/main/java/com/hugh/libwebrtc/RtcActivity.java
@@ -1,7 +1,28 @@
 package com.hugh.libwebrtc;
 
+import android.content.res.AssetManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.concurrent.ExecutorService;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +30,280 @@ import androidx.appcompat.app.AppCompatActivity;
 /**
  * Created by chenyw on 2020/7/24.
  */
-public class RtcActivity extends AppCompatActivity {
+public class RtcActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int SAMPLERATE_32K = 32000;
+    private static final int SAMPLERATE_16K = 16000;
+    private static final int SAMPLERATE_8K = 8000;
+    private static final int SAMPLERATE_44K = 44100;
+
+    private static final String AUDIO_FILE_AST_8K = "record/recorded_audio.pcm";
+    private static final String AUDIO_FILE_AST_16k = "record/recorded_audio_16k.pcm";
+    private static final String AUDIO_FILE_AST_32k = "record/recorded_audio_32k.pcm";
+    private static final String AUDIO_FILE_AST_44k = "record/recorded_audio_44k.pcm";
+    //    private static final String AUDIO_FILE_AST_32k = "record/test_32k.pcm";
+    private static final String AUDIO_FILE_AST_32k_TEST = "record/recorded_audio_fun.pcm";
+
+    /**
+     * 原始音频文件路径
+     */
+    private static final String AUDIO_FILE_PATH_8k = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio.pcm";
+    private static final String AUDIO_FILE_PATH_16k = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_16k.pcm";
+    private static final String AUDIO_FILE_PATH_32K = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_32k.pcm";
+    private static final String AUDIO_FILE_PATH_44K = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_44k.pcm";
+    //    private static final String AUDIO_FILE_PATH_32K = Environment.getExternalStorageDirectory().getPath() +
+    //            "/test_32k.pcm";
+    private static final String AUDIO_FILE_PATH_32K_TEST = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_fun.pcm";
+    /**
+     * 处理过的音频文件路径
+     */
+    private static final String AUDIO_PROCESS_FILE_PATH_8k = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_process.pcm";
+    private static final String AUDIO_PROCESS_FILE_PATH_16k = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_process_16k.pcm";
+    private static final String AUDIO_PROCESS_FILE_PATH_32k = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_process_32k.pcm";
+    //    private static final String AUDIO_PROCESS_FILE_PATH_32k = Environment.getExternalStorageDirectory().getPath
+    //    () +
+    //            "/test_process_32k.pcm";
+    private static final String AUDIO_PROCESS_FILE_PATH_44k = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_process_44k.pcm";
+    private static final String AUDIO_PROCESS_FILE_PATH_32k_TEST = Environment.getExternalStorageDirectory().getPath() +
+            "/recorded_audio_process_fun.pcm";
+
+
+    private Button mBtnNsOperate;
+    private boolean isProcessing;
+    private boolean isInitialized;
+    private int mMinBufferSize;
+    private AudioTrack mAudioTrack;
+    private File mFile;
+    private File mProcessFile;
+    private String AUDIO_FILE_PATH;
+    private String AUDIO_PROCESS_FILE_PATH;
+    private String srcPath;
+    private boolean process32KData;
+    private int mSampleRate;
+    private ExecutorService mThreadExecutor;
+    private int selectId = -1;
+    private boolean isPlaying;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.module_rtc_activity_main);
+        mBtnNsOperate = findViewById(R.id.ns_audio);
+        mBtnNsOperate.setOnClickListener(this);
+        selectId = R.id.rb_8k;
+        RadioGroup radioGroup = findViewById(R.id.rg);
+        int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+        switchDataSrc(checkedRadioButtonId);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switchDataSrc(checkedId);
+            }
+        });
     }
+
+    private void switchDataSrc(int rbId) {
+        if (selectId == rbId) {
+            return;
+        }
+        isInitialized = false;
+        selectId = rbId;
+        process32KData = false;
+        if (rbId == R.id.rb_8k) {
+            mSampleRate = SAMPLERATE_8K;
+            AUDIO_FILE_PATH = AUDIO_FILE_PATH_8k;
+            AUDIO_PROCESS_FILE_PATH = AUDIO_PROCESS_FILE_PATH_8k;
+            srcPath = AUDIO_FILE_AST_8K;
+        } else if (rbId == R.id.rb_16k) {
+            mSampleRate = SAMPLERATE_16K;
+            AUDIO_FILE_PATH = AUDIO_FILE_PATH_16k;
+            AUDIO_PROCESS_FILE_PATH = AUDIO_PROCESS_FILE_PATH_16k;
+            srcPath = AUDIO_FILE_AST_16k;
+        } else if (rbId == R.id.rb_32k) {
+            process32KData = true;
+            mSampleRate = SAMPLERATE_32K;
+            AUDIO_FILE_PATH = AUDIO_FILE_PATH_32K;
+            AUDIO_PROCESS_FILE_PATH = AUDIO_PROCESS_FILE_PATH_32k;
+            srcPath = AUDIO_FILE_AST_32k;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mBtnNsOperate) {
+
+        }
+    }
+
+    private void initAudio() {
+        if (TextUtils.isEmpty(srcPath) || TextUtils.isEmpty(AUDIO_FILE_PATH) || TextUtils.isEmpty(AUDIO_PROCESS_FILE_PATH)) {
+            return;
+        }
+        Log.e("sws", "srcPath==" + srcPath);
+        Log.e("sws", "AUDIO_PROCESS_FILE_PATH==" + AUDIO_PROCESS_FILE_PATH);
+        Log.e("sws", "AUDIO_FILE_PATH==" + AUDIO_FILE_PATH);
+
+        mProcessFile = new File(AUDIO_PROCESS_FILE_PATH);
+
+        mFile = new File(AUDIO_FILE_PATH);
+
+        if (!mFile.exists() || mFile.length() <= 0) {
+            Log.e("sws", " init file-----------");
+            mThreadExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    AssetManager assets = getAssets();
+                    try {
+                        InputStream inputStream = assets.open(srcPath);
+                        FileOutputStream fileOutputStream = new FileOutputStream(mFile);
+                        byte[] buf = new byte[1024 * 1024];
+                        int len;
+                        while ((len = inputStream.read(buf)) != -1) {
+                            fileOutputStream.write(buf, 0, len);
+                        }
+                        inputStream.close();
+                        fileOutputStream.close();
+                        isInitialized = true;
+                        Log.e("sws", " init file end-----------");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } else {
+            Log.e("sws", "-----------");
+            isInitialized = true;
+        }
+    }
+
+    private void initAudioRecord() {
+        stopPlay();
+        mMinBufferSize = AudioRecord.getMinBufferSize(mSampleRate, AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+        if (mAudioTrack == null) {
+            mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, mSampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, mMinBufferSize, AudioTrack.MODE_STREAM);
+        }
+    }
+
+    private void stopPlay() {
+        isPlaying = false;
+        if (mAudioTrack != null) {
+            mAudioTrack.release();
+            mAudioTrack = null;
+        }
+    }
+
+    private void process() {
+        if (isProcessing) {
+            return;
+        }
+        isProcessing = true;
+        mThreadExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+//                WebRtcUtils.webRtcAgcInit(0, 255, mSampleRate);
+                WebRtcUtils.webRtcNsInit(mSampleRate);
+
+                Log.e("sws", "====mSampleRate=" + mSampleRate + ": process32KData=" + process32KData);
+                FileInputStream ins = null;
+                FileOutputStream out = null;
+                try {
+                    File inFile = mFile;
+                    ins = new FileInputStream(inFile);
+                    File outFile = new File(AUDIO_PROCESS_FILE_PATH);
+                    out = new FileOutputStream(outFile);
+
+                    byte[] buf;
+                    if (process32KData) {
+                        //TODO
+                        /*
+                         * 测试发现，32k采样率,数据buf越少，增益后可能有滋滋的声音
+                         *
+                         */
+                        buf = new byte[640 * 40];
+                    } else {
+                        buf = new byte[320];
+                    }
+                    while (ins.read(buf) != -1) {
+                        short[] shortData = new short[buf.length >> 1];
+
+                        short[] processData = new short[buf.length >> 1];
+
+                        ByteBuffer.wrap(buf).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shortData);
+
+                        if (process32KData) {
+                            //    short[] nsProcessData =shortData;
+//                            short[] nsProcessData = WebRtcUtils.webRtcNsProcess32k(shortData.length, shortData);
+//                            WebRtcUtils.webRtcAgcProcess32k(nsProcessData, processData, nsProcessData.length);
+//                            out.write(shortsToBytes(processData));
+                        } else {
+                            short[] nsProcessData;
+                            if (selectId == R.id.rb_16k) {
+                                nsProcessData = WebRtcUtils.webRtcNsProcess(mSampleRate, shortData.length, shortData);
+//                                WebRtcUtils.webRtcAgcProcess(nsProcessData, processData, shortData.length);
+                                out.write(shortsToBytes(nsProcessData));
+                            } else if (selectId == R.id.rb_8k) {
+                                nsProcessData = WebRtcUtils.webRtcNsProcess(mSampleRate, shortData.length, shortData);
+//                                WebRtcUtils.webRtcAgcProcess(nsProcessData, processData, nsProcessData.length);
+                                out.write(shortsToBytes(nsProcessData));
+                            }
+                        }
+
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "处理完成", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    isProcessing = false;
+                    WebRtcUtils.webRtcNsFree();
+                    if (out != null) {
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (ins != null) {
+                        try {
+                            ins.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                Log.e("sws", "ns end======");
+            }
+        });
+
+    }
+
+    private byte[] shortsToBytes(short[] data) {
+        byte[] buffer = new byte[data.length * 2];
+        int shortIndex, byteIndex;
+        shortIndex = byteIndex = 0;
+        for (; shortIndex != data.length; ) {
+            buffer[byteIndex] = (byte) (data[shortIndex] & 0x00FF);
+            buffer[byteIndex + 1] = (byte) ((data[shortIndex] & 0xFF00) >> 8);
+            ++shortIndex;
+            byteIndex += 2;
+        }
+        return buffer;
+    }
+
 }
